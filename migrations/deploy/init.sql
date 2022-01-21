76cc2a49f2c71990a4192ef38809649db4beb852
patricey0 <patrice.yolle@gmail.com>
2022-01-21 20:54:14 +0100

    update deploy

--- migrations/deploy/init.sql
+++ migrations/deploy/init.sql
@@ -12,22 +12,22 @@ CREATE TABLE visitor (
     isAdmin BOOLEAN NOT NULL DEFAULT false
 );
 
--- CREATE TABLE announce (
---     id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
---     title TEXT NOT NULL,
---     image TEXT NOT NULL,
---     creation_date TIMESTAMPTZ NOT NULL DEFAULT NOW(),
---     update_date TIMESTAMPTZ NULL,
---     description TEXT NOT NULL,
---     category TEXT NOT NULL,
---     visitor_id INT NOT NULL REFERENCES "visitor"(id)
--- );
+CREATE TABLE announce (
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL,
+    image TEXT NOT NULL,
+    creation_date TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    update_date TIMESTAMPTZ NULL,
+    description TEXT NOT NULL,
+    category TEXT NOT NULL,
+    visitor_id INT NOT NULL REFERENCES "visitor"(id)
+);
 
--- CREATE TABLE bookmarked (
---     "visitor_id" INT NOT NULL REFERENCES "visitor"(id),
---     "announce_id" INT NOT NULL REFERENCES announce(id),
---     added_date TIMESTAMPTZ NOT NULL DEFAULT NOW(),
---     PRIMARY KEY ("visitor_id", "announce_id")
--- );
+CREATE TABLE bookmarked (
+    "visitor_id" INT NOT NULL REFERENCES "visitor"(id),
+    "announce_id" INT NOT NULL REFERENCES announce(id),
+    added_date TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    PRIMARY KEY ("visitor_id", "announce_id")
+);
 
 COMMIT;
