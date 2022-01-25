324040578f9c2a613c27c23e4ab1b02dc5bacded
vincentg2 <vincentguyot2@gmail.com>
2022-01-25 15:01:09 +0100

    quick fix footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,27 +1,84 @@
-// == Import
-import './styles.scss';
+import {
+  Box,
+  Container,
+  Stack,
+  Text,
+  Image,
+  useColorModeValue,
+  Link,
+} from '@chakra-ui/react';
 import { NavLink } from 'react-router-dom';
+import logo from 'src/assets/img/spider-plant.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-// == Composant
-const Footer = () => (
-  <footer>
-    <div>
-      <ul className="navfooter">
-        <li><NavLink to="/" className="navfooter__item">Home</NavLink></li>
-        <li><NavLink to="/annonces" className="navfooter__item">Annonces</NavLink></li>
-        <li><NavLink to="/tuto" className="navfooter__item">Tuto</NavLink></li>
-        <li><NavLink to="/contact" className="navfooter__item">Contact</NavLink></li>
-      </ul>
-    </div>
-    <div className="credential">
-      <p>Mentions légales @ 2022 Shokubutsu</p>
-    </div>
-    <div className="social">
-      <p> Vous pouvez aussi nous retrouvez sur <FontAwesomeIcon icon={faInstagram} /></p>
-    </div>
-  </footer>
-);
+// import { FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa';
+import { faInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 
-// == Export
-export default Footer;
+export default function Footer() {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+  return (
+    <Box
+      bg={useColorModeValue('gray.50', 'gray.900')}
+      color={useColorModeValue('gray.700', 'gray.200')}
+    >
+      <Container
+        as={Stack}
+        maxW="6xl"
+        py={4}
+        spacing={4}
+        justify="center"
+        align="center"
+      >
+        <Image
+  // borderRadius='full'
+          boxSize="50px"
+          src={logo}
+          alt="logo shokubutsu"
+        />
+        <Stack direction="row" spacing={6}>
+          <NavLink to="/" onClick={scrollToTop}>Home</NavLink>
+          <NavLink to="/annonces">Annonces</NavLink>
+          <NavLink to="/guide">Guide</NavLink>
+          <NavLink to="/contact" onClick={scrollToTop}>Contact</NavLink>
+        </Stack>
+      </Container>
+
+      <Box
+        borderTopWidth={1}
+        borderStyle="solid"
+        borderColor={useColorModeValue('gray.200', 'gray.700')}>
+        <Container
+          as={Stack}
+          maxW="6xl"
+          py={4}
+          direction={{ base: 'column', md: 'row' }}
+          spacing={4}
+          justify={{ base: 'center', md: 'space-between' }}
+          align={{ base: 'center', md: 'center' }}
+        >
+          <Text>© 2022 Shokubutsu. All rights reserved</Text>
+          <Stack direction="row" spacing={6}>
+            <Media />
+          </Stack>
+        </Container>
+      </Box>
+    </Box>
+  );
+}
+const Media = () => (
+  <>
+    <Link href="https://instagram.com" isExternal>
+      <FontAwesomeIcon icon={faInstagram} />
+    </Link>
+    <Link href="https://twitter.com" isExternal>
+      <FontAwesomeIcon icon={faTwitter} />
+    </Link>
+    <Link href="https://youtube.com" isExternal>
+      <FontAwesomeIcon icon={faYoutube} />
+    </Link>
+  </>
+);
