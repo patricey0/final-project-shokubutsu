6c8a4c742da33db7c44f72490f03759e06d32aba
myriamchaibi <myriam.ci@live.fr>
2022-01-26 15:28:16 +0100

    Add Grid template into Annouces

--- src/components/Announces/index.js
+++ src/components/Announces/index.js
@@ -1,11 +1,16 @@
 // == Import
+import { SimpleGrid, Box } from '@chakra-ui/react'
 import './styles.scss';
 
 // == Composant
 const Announces = () => (
-  <div>
-    <p>Composant : Announces</p>
-  </div>
+  <SimpleGrid columns={{sm: 1, md: 2}} spacing={10}>
+    <Box bg='tomato' height='80px'></Box>
+    <Box bg='tomato' height='80px'></Box>
+    <Box bg='tomato' height='80px'></Box>
+    <Box bg='tomato' height='80px'></Box>
+    <Box bg='tomato' height='80px'></Box>
+  </SimpleGrid>
 );
 
 // == Export
