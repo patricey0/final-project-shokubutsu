e32962ec00890c117fd5be52044a19796e8f3aab
vincentg2 <vincentguyot2@gmail.com>
2022-01-26 19:39:50 +0100

    presentation try

--- src/components/MyFavorites/index.js
+++ src/components/MyFavorites/index.js
@@ -1,10 +1,61 @@
 // == Import
+import {
+  Flex,
+  Stack,
+  Text,
+  VStack,
+  useBreakpointValue,
+  // useColorModeValue,
+} from '@chakra-ui/react';
+import bcgImg from 'src/assets/img/plant-8.jpg';
 import './styles.scss';
 
 // == Composant
 const MyFavorites = () => (
-  <div className="div">
-    <p>Composant : MyFavorites</p>
+  <div>
+    <Flex
+      w="full"
+      h="100vh"
+      backgroundImage={
+        bcgImg
+      }
+      backgroundSize="cover"
+      backgroundPosition="center center"
+    >
+      <VStack
+        w="full"
+        justify="center"
+        px={useBreakpointValue({ base: 4, md: 8 })}
+        bgGradient="linear(to-r, blackAlpha.600, transparent)"
+      >
+        <Stack
+          maxW="2xl"
+          align="center"
+          spacing={6}
+          borderRadius="2em"
+          bg="#badec7"
+        >
+          <Text
+            color="white"
+            fontWeight={700}
+            lineHeight={1.2}
+            p={4}
+            fontSize={useBreakpointValue({ base: '2xl', md: '3xl' })}
+          >
+            La page des favoris de l'utilisateur est encore en cours de construction...
+          </Text>
+          <Text
+            color="white"
+            fontWeight={700}
+            lineHeight={1.2}
+            p={4}
+            fontSize={useBreakpointValue({ base: '2xl', md: '3xl' })}
+          >
+            On repasse au prochain sprint ?
+          </Text>
+        </Stack>
+      </VStack>
+    </Flex>
   </div>
 );
 
