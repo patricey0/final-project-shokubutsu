670eff19a4974a3bab0e0e10fc1691a3977f92a1
myriamchaibi <myriam.ci@live.fr>
2022-01-28 14:42:20 +0100

    Remove bgImage in Announce

--- src/components/Announces/Announce/index.js
+++ src/components/Announces/Announce/index.js
@@ -91,10 +91,6 @@ function Announce() {
           </Box>
           <Box zIndex="1" width="100%" position="absolute" height="100%">
             <Box
-              bgGradient={useColorModeValue(
-                'radial(orange.600 1px, transparent 1px)',
-                'radial(orange.300 1px, transparent 1px)'
-              )}
               backgroundSize="20px 20px"
               opacity="0.4"
               height="100%"
