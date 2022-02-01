e7fbd95d28760ad0708e43b4e571e285df3c9976
patricey0 <patrice.yolle@gmail.com>
2022-02-01 11:11:28 +0100

    update userController to return error correctly

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -95,7 +95,7 @@ const userController = {
             res.json(user);
         } catch (error) {
             console.log(error);
-            res.status(500).json({"message":error});
+            res.status(500).json(error);
         }
     },
 
