734df20b85a11471b622d6355669e6015899b9c9
patricey0 <patrice.yolle@gmail.com>
2022-02-01 19:39:23 +0100

    correction image model

--- app/models/image.js
+++ app/models/image.js
@@ -4,11 +4,7 @@ class Image extends CoreModel {
     async updateImageVisitor() {
         try {
             console.log(`update image visitor`);
-            const data = {
-                "id":this.userId,
-                "picture":this.image_url
-            };
-            return new Image(await CoreModel.getRow('SELECT * FROM update_image_visitor($1)', [data]));
+            return new Image(await CoreModel.getRow('SELECT * FROM update_image_visitor($1)', [this]));
         } catch (error) {
             console.log(error);
             if (error.detail) {
@@ -21,10 +17,6 @@ class Image extends CoreModel {
     async updateImageAnnounce() {
         try {
             console.log('update image announce');
-            // const data = {
-            //     "id":this.userId,
-            //     "picture":this.image_url
-            // };
             console.log(this);
             return new Image(await CoreModel.getRow('SELECT * FROM update_image_announce($1)', [this]));
         } catch (error) {
