4d3cda8994fb6e57950da239ad678b203ef7d45a
myriamchaibi <myriam.ci@live.fr>
2022-02-02 11:58:41 +0100

    Add style CreateAnnounce

--- src/components/CreateAnnounce/index.js
+++ src/components/CreateAnnounce/index.js
@@ -10,6 +10,8 @@ import {
   Container,
   Box,
   Select,
+  Textarea,
+  Heading,
 } from '@chakra-ui/react';
 import './styles.scss';
 
@@ -22,32 +24,39 @@ const CreateAnnounce = () => {
   };
 
   return (
-    <Container >
-      <Box padding='4'bg="gray.300" >
-        <form action="" onSubmit={handleSubmit}>
-          <FormControl color="black" as='fieldset' border="black">
-            <FormLabel htmlFor='exchange'>Type d'échange</FormLabel>
-            <Select id='exchange'>
-              <option>Don</option>
-              <option>Echange</option>
-            </Select>
-            <FormLabel htmlFor='title'>Titre</FormLabel>
-            <Input id='title' type='text' />
-            <FormLabel htmlFor='description'>Description</FormLabel>
-            <Input id='description' type='text' />
-            <FormLabel htmlFor='picture'>Ajouter une photo</FormLabel>
-            <Input id='picture' type='file' />
-            <Button
-                mt={4}
-                colorScheme='teal'
-                type='submit'
-              >
-                Soumettre
-            </Button>
-          </FormControl>
-        </form>
-      </Box>
-    </Container>
+    <div className="div">
+      <Heading color="#366D4B" pt={10}>Ajouter une annonce</Heading>
+      <Container>
+        <Box padding='4'bg="" >
+          <form action="" onSubmit={handleSubmit}>
+            <FormControl color="black" as='fieldset' border="black">
+              <FormLabel htmlFor='exchange' pt={3}>Type d'échange</FormLabel>
+              <Select id='exchange'>
+                <option>Don</option>
+                <option>Echange</option>
+              </Select>
+              <FormLabel htmlFor='title' pt={3}>Titre</FormLabel>
+              <Input id='title' type='text' placeholder="Cactus .."/>
+              <FormLabel htmlFor='description' pt={3}>Description</FormLabel>
+              {/* <Input id='description' type='text'/> */}
+              <Textarea placeholder="Plante d'intérieur, extérieur .." />
+              <FormLabel htmlFor='picture' pt={3}>Ajouter une photo</FormLabel>
+              <Input id='picture' type='file' p={3} h="none"/>
+              <Button
+                  m={8}
+                  color="white"
+                  bgColor="#366D4B"
+                  type='submit'
+                  _hover={{bgColor:"#366D4B"}}
+                  _active={{bgColor:"#BEE0CA", color:"#366D4B"}}
+                >
+                  Soumettre
+              </Button>
+            </FormControl>
+          </form>
+        </Box>
+      </Container>
+    </div>
   );
 
 };
